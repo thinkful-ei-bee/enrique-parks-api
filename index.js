b5e0909065eb1aeeb336e51982a946f5b92682f4
Enrique Montemayor <eemontemayor@live.com>
2019-03-02 19:14:35 -0600

    modifying to make sure it doesn't exceed maxResult

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 
-// Requirements:
-// The user must be able to search for parks in one or more states.
-// The user must be able to set the max number of results, with a default of 10.
-// The search must trigger a call to NPS's API.
-// The parks in the given state must be displayed on the page. Include at least:
-// Full name
-// Description
-// Website URL
-// The user must be able to make multiple searches and see only the results for the current search.
-
 
 'use strict';
 
@@ -26,7 +16,7 @@ function watchForm() {
   }
 
   function getParks(state, maxResults=10) {
-
+// FINISH WITH USING encodeURIComponent or something else to make sure it doesnt show more than maxResult
     const url = `${searchURL}limit=${maxResults}&q=${state}&api_key=${apiKey}`;
   
     fetch(url)
